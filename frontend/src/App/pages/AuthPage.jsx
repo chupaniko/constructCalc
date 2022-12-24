4280eab204c99451b876d63ea55b3680d7d0a89d
Vladislav <Taimkraft@mail.ru>
2022-12-24 12:02:58 +0400

    CalculationScreen: final fix authorization

--- frontend/src/App/pages/AuthPage.jsx
+++ frontend/src/App/pages/AuthPage.jsx
@@ -8,6 +8,7 @@ import { apiAuth } from "../../api/api";
 const AuthPage = () => {
   const [login, setLogin] = React.useState("");
   const [password, setPassword] = React.useState("");
+  const [messageError, setmessageError] = React.useState(false);
 
   let navigate = useNavigate();
 
@@ -21,103 +22,107 @@ const AuthPage = () => {
     });
 
     apiAuth(json)
-      .then((response) => {
-        sessionStorage.setItem("username", response.data["username"]);
-        sessionStorage.setItem("userId", response.data["id"]);
+        .then((response) => {
+          sessionStorage.setItem("username", response.data["username"]);
+          sessionStorage.setItem("userId", response.data["id"]);
 
-        navigate("/clients");
-      })
-      .catch((error) => {
-        console.error(error);
+          navigate("/clients");
+        })
+        .catch((error) => {
+          console.error(error);
 
-        if (error.response) {
-          sessionStorage.setItem("username", "");
-          sessionStorage.setItem("userId", "");
-        }
-      });
+          if (error.response) {
+            sessionStorage.setItem("username", "");
+            sessionStorage.setItem("userId", "");
+          }
+
+          setmessageError(true);
+        });
   }
 
   return (
-    <Pane
-      height={"100%"}
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-    >
-      <Card
-        height={600}
-        width={1200}
-        background={"white"}
-        borderRadius={16}
-        display="flex"
-        boxShadow="4px 4px 4px rgba(0, 0, 0, 0.25)"
-      >
-        <Pane
-          flex="1"
-          backgroundImage={`url(${BuildingImage})`}
-          backgroundSize="cover"
-          borderBottomLeftRadius={16}
-          borderTopLeftRadius={16}
-        />
-
-        <Pane
+      <Pane
+          height={"100%"}
           display="flex"
-          flex="1"
-          flexDirection="column"
-          gap={32}
           justifyContent="center"
+          alignItems="center"
+      >
+        <Card
+            height={600}
+            width={1200}
+            background={"white"}
+            borderRadius={16}
+            display="flex"
+            boxShadow="4px 4px 4px rgba(0, 0, 0, 0.25)"
         >
-          <Pane textAlign="center">
-            <img src={logo} alt="Logo" />
+          <Pane
+              flex="1"
+              backgroundImage={`url(${BuildingImage})`}
+              backgroundSize="cover"
+              borderBottomLeftRadius={16}
+              borderTopLeftRadius={16}
+          />
 
-            <Heading
-              is="h1"
-              className="title"
-              fontSize={44}
-              fontWeight={700}
-              lineHeight="52px"
-            >
-              Название
-            </Heading>
-          </Pane>
+          <Pane
+              display="flex"
+              flex="1"
+              flexDirection="column"
+              gap={32}
+              justifyContent="center"
+          >
+            <Pane textAlign="center">
+              <img src={logo} alt="Logo" />
 
-          <form className="login-form" onSubmit={handleSubmit}>
-            <Pane display="flex" flexDirection="column" alignItems="center">
-              <TextInputField
-                placeholder="Логин"
-                isInvalid={!login}
-                validationMessage={!login ? "Введите логин" : false}
-                value={login}
-                onChange={(e) => setLogin(e.target.value)}
-                marginBottom={4}
-                width={412}
-              />
-
-              <TextInputField
-                placeholder="Пароль"
-                isInvalid={!password}
-                validationMessage={!password ? "Введите пароль" : false}
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                marginTop={0}
-                width={412}
-              />
-
-              <Button
-                appearance="primary"
-                type="submit"
-                height={40}
-                width={160}
-                disabled={!login || !password}
+              <Heading
+                  is="h1"
+                  className="title"
+                  fontSize={44}
+                  fontWeight={700}
+                  lineHeight="52px"
               >
-                Войти
-              </Button>
+                НеСтройСам
+              </Heading>
             </Pane>
-          </form>
-        </Pane>
-      </Card>
-    </Pane>
+
+            <form className="login-form" onSubmit={handleSubmit}>
+              <Pane display="flex" flexDirection="column" alignItems="center">
+                <TextInputField
+                    placeholder="Логин"
+                    isInvalid={!login}
+                    validationMessage={!login ? "Введите логин" : false}
+                    value={login}
+                    onChange={(e) => setLogin(e.target.value)}
+                    marginBottom={4}
+                    width={412}
+                />
+
+                <TextInputField
+                    placeholder="Пароль"
+                    isInvalid={!password}
+                    validationMessage={!password ? "Введите пароль" : false}
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    marginTop={0}
+                    width={412}
+                />
+
+                {messageError===true && <Pane marginTop={-25}><p style={{color:'red'}}>Неверно введён логин или пароль</p></Pane> }
+
+                <Button
+                    appearance="primary"
+                    type="submit"
+                    height={40}
+                    width={160}
+                    disabled={!login || !password}
+                >
+                  Войти
+                </Button>
+              </Pane>
+            </form>
+          </Pane>
+        </Card>
+      </Pane>
   );
 };
 
